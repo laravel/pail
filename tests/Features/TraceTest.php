7251090589f60a0e207261b4e5b3eceab1130b9b
Nuno Maduro <enunomaduro@gmail.com>
2023-09-26 13:39:24 +0100

    Adjusts test output when verbosity

--- tests/Features/TraceTest.php
+++ tests/Features/TraceTest.php
@@ -12,7 +12,7 @@ test('does not show trace by default', function () {
 
 test('does show trace when verbose', function () {
     expect('throw new Exception("my exception message")')->toPail(<<<'EOF'
-        ┌ 03:04:05 Exception ───────── app/MyClass.php:12
+        ┌ 2024-01-01 03:04:05 Exception  app/MyClass.php:12
         │ my exception message
         │ 1. app/MyClass.php:12
         │ 2. app/MyClass.php:34
