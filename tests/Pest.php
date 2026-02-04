ad6318e25744e57ad593307563792fc28059eb6e
Mior Muhammad Zaki <crynobone@gmail.com>
2026-02-04 18:55:54 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Pest.php
+++ tests/Pest.php
@@ -74,7 +74,7 @@ expect()->extend('toPail', function (string $expectedOutput, array $options = []
             remote(['eval', ProcessUtils::escapeArgument(base64_encode($code.';'))])->run();
         });
 
-    usleep(10);
+    usleep(100);
 
     $output = $process->getOutput();
     $output = preg_replace('/\e\[[\d;]*m/', '', $output);
